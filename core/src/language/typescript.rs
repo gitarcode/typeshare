2e2b203dd27fdc5f6eb69501a544323d26baada5
Naveen Narayanan <naveen@gitar.co>
2024-11-06 14:39:22 -0800

    [typeshare] Support linux-arm64 and support u64 (#1)
    ## Description
    * Switching build over to `zigbuild` to support linux arm64 variant
    * Porting over https://github.com/1Password/typeshare/pull/140 to support large number
    * Adding some default overrides to include base types for stuff such as `HashSet`
    
    ## Test Plan
    * Typeshare built for repo
    
    ## Revert Plan
    * Revert

--- core/src/language/typescript.rs
+++ core/src/language/typescript.rs
@@ -80,9 +80,8 @@ impl Language for TypeScript {
             SpecialRustType::U64
             | SpecialRustType::I64
             | SpecialRustType::ISize
-            | SpecialRustType::USize => {
-                panic!("64 bit types not allowed in Typeshare")
-            }
+            | SpecialRustType::U128
+            | SpecialRustType::USize => Ok("number".into())
         }
     }
 
@@ -178,8 +177,9 @@ impl Language for TypeScript {
         w: &mut dyn Write,
         imports: ScopedCrateTypes<'_>,
     ) -> std::io::Result<()> {
+        writeln!(w, "import type {{ HashSet }} from \"./base\";")?;
         for (path, ty) in imports {
-            write!(w, "import {{ ")?;
+            write!(w, "import type {{ ")?;
             let ty_list = ty.iter().join(", ");
             write!(w, "{ty_list}")?;
             writeln!(w, " }} from \"./{path}\";")?;
